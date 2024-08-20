c309ef988d799628ef93675c649fe7254dfde5e2
JoaoMoraisxD <joaomoraisxd@gmailcom>
2024-08-19 22:57:29 -0300

    Feat: criando interface que solicita senha do usuário para acesso ao bacno

---
+++ frontend/javascript/form.js
@@ -0,0 +1,6 @@
+let submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', async () => { 
+   let nomeUsuario = document.getElementsByClassName('user')[0].value;
+   let senhaDb = document.getElementsByClassName('senhaDb')[0].value;
+})
